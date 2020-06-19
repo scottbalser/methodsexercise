952965be0835110c4f0c8b1648a7c9bb63e58cd2
scott balser <scottbalser@gmail.com>
2020-06-19 13:12:51 -0500

    method exercise story

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Whats your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Where do you live?");
+            string userHome = Console.ReadLine();
+
+            Console.WriteLine("Whats your favorite sport?");
+            string userSport = Console.ReadLine();
+
+            Console.WriteLine("Whats your favorite team?");
+            string userFavteam = Console.ReadLine();
+
+            Console.WriteLine($"{userName} lives in {userHome}");
+            Console.WriteLine($"{userName} loves to watch {userSport}");
+            Console.WriteLine($" GO {userFavteam}");
+
+
+
+
+
+
+        }
+    }
+}
